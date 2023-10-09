913a1589aff7212aa71ad1748d61e6bf088d2362
Anton Zlygostev <zlygostev@hotmail.com>
2023-10-09 19:37:40 +0700

    Initial commit

---
+++ ConsoleExt.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+
+namespace DetachedConsoleDemo
+{
+    internal static class ConsoleExt
+    {
+        public static bool Free() => FreeConsole();
+
+        public static bool Alloc() => AllocConsole();
+
+        public static uint ConsoleOwnerProcessId
+        {
+            get
+            {
+                GetWindowThreadProcessId(GetConsoleWindow(), out var pid);
+                return pid;
+            }
+        }
+
+
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        private static extern bool FreeConsole();
+        [DllImport("kernel32.dll", SetLastError = true, ExactSpelling = true)]
+        private static extern bool AllocConsole();
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetConsoleWindow();
+        [DllImport("user32.dll", SetLastError = true)]
+        private static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DetachedConsoleDemo;
+using System.Diagnostics;
+
+if (ConsoleExt.ConsoleOwnerProcessId != Process.GetCurrentProcess().Id)
+{
+    ConsoleExt.Free();
+    ConsoleExt.Alloc();
+}
+
+Console.WriteLine("Hello, World!");
+Console.ReadLine();
